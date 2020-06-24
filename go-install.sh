ed9102798e23eb5e972b5c686f01c7a0cc151d87
root <root@centos8.linuxvmimages.com>
2020-06-24 10:48:51 -0400

    updated go-install.sh

---
+++ go-install.sh
@@ -0,0 +1,17 @@
+echo 'installing and configuring go'
+wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+gunzip go1.14.4.linux-amd64.tar.gz
+tar -xvf go1.14.4.linux-amd64.tar
+mv go /usr/local/
+sleep 60
+mkdir $HOME/go
+sed -i '$ a export GOPATH="$HOME/go'" ~/.bashrc
+sed -i '$ a export PATH="$PATH:/usr/local/go/bin"' ~/.bashrc
+sed -i '$ a export GOPATH="$HOME/go'"  ~/.bashrc
+. ~/.bashrc
+go get -u -v golang.org/x/crypto/ssh
+go get -u -v github.com/hashicorp/terraform
+go get -u -v github.com/josenk/terraform-provider-esxi
+cd $GOPATH/src/github.com/josenk/terraform-provider-esxi
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -extldflags "-static"' -o terraform-provider-esxi_`cat version`
+echo 'go installed and configured'
