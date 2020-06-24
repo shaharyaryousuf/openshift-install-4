95532f33302a0e912c168319c78b065d1dcab766
root <root@centos8.linuxvmimages.com>
2020-06-24 10:54:11 -0400

    added prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+d /root/ocp67
+openshift-install create ignition-configs
+openshift-install create manifests
+sed -i '' 's/mastersSchedulable: true/mastersSchedulable: false/'  manifests/cluster-scheduler-02-config.yml
+openshift-install create ignition-configs
+cp *.ign /var/lib/matchbox/ignition
